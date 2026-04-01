f996590f9b8e418259aa63d29943e0fdfbe8cdcc
smuratbecova <smuratbecova@gmail.com>
2026-04-01 20:27:05 +0600

    lesson_1

---
+++ lesson_1.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+class Hero:
+    def __init__(self, name, hp, lvl):
+        self.name = name
+        self.hp = hp
+        self.lvl =lvl
+    def action(self):
+        return f"{self,name} ero base action"
+
+kirito = Hero("Kirito", 1000, 100)
+# print(kirito.hp)
